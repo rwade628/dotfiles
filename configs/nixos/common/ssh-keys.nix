9d190be40b98c905199bad4a2ae54addbc71701b
Ryan Wade <ryanwade628@gmail.com>
2026-02-10 16:06:48 -0500

    Update authorized keys

--- configs/nixos/common/ssh-keys.nix
+++ configs/nixos/common/ssh-keys.nix
@@ -1,7 +1,6 @@
 # SSH public keys for all machines
 {
   sshKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC90KqGLJG4vaYYes3dDwD46Ui3sDiExPTbL7AkYg7i9 bas@nijho.lt"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEMRmEP/ZUShYdZj/h3vghnuMNgtWExV+FEZHYyguMkX basnijholt@blink"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL77MQ/xuw02PjTgvE3tjFElr99SYsqFmO6WxHjFViBD ryan"
   ];
 }
